7e2153641b15a49118f649c24ad874ab5bb5df63
Shubham <shubhamkharche2003@gmail.com>
2025-07-20 15:37:59 +0530

    Project setup completed

---
+++ src/main/java/com/shubham/Student/Course/Enrollment/System/StudentCourseEnrollmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.Student.Course.Enrollment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseEnrollmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseEnrollmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/Student/Course/Enrollment/System/StudentCourseEnrollmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.Student.Course.Enrollment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseEnrollmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
